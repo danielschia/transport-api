360f81fe417763fbea6abbf514888a0b35102e85
Daniel Schiavoni <daniel.schiavoni@leanbakery.com>
2022-04-22 15:52:11 +0100

    scaffold operations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ApplicationRecord
   has_many :facilities
+  has_many :contacts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_145001) do
+ActiveRecord::Schema.define(version: 2022_04_22_145120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 2022_04_22_145001) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_facilities_on_customer_id"
+  end
+
+  create_table "operations", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "contacts", "customers"
