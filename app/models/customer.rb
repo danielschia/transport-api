ed4c48831f13b4d858b35fd5bdd0e7e122b218e1
Daniel Schiavoni <daniel.schiavoni@leanbakery.com>
2022-04-25 14:13:01 +0100

    add relations to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
-  has_many :facilities
-  has_many :contacts
+  has_and_belongs_to_many :operations, dependent: :destroy
+  has_many :facilities, dependent: :destroy
+  has_many :contacts, dependent: :destroy
+  has_many :operations, dependent: :destroy
 end
