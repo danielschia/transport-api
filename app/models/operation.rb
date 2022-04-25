8e8bdbc0630852648c9ff26e4df3afc64bfe04fa
Daniel Schiavoni <daniel.schiavoni@leanbakery.com>
2022-04-25 19:39:49 +0100

    adds validation to operation

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,10 @@
 class Operation < ApplicationRecord
   has_and_belongs_to_many :customers
+
+  validates :description, presence: { message: "Description Required!"}
+  validates :description, uniqueness: {
+    message: -> (object, data) do
+      "Operation '#{data[:value]}' duplicated!"
+    end 
+  }
 end
