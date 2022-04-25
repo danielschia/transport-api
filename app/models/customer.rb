aaef695036b9607c3754358fa99800f1a2286edf
Daniel Schiavoni <daniel.schiavoni@leanbakery.com>
2022-04-25 14:43:22 +0100

    fix order of relations in customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,8 @@
 class Customer < ApplicationRecord
-  has_and_belongs_to_many :operations, dependent: :destroy
   has_many :facilities, dependent: :destroy
   has_many :contacts, dependent: :destroy
   has_many :operations, dependent: :destroy
+
+  has_and_belongs_to_many :operations, dependent: :destroy
   attribute :operation_ids, dependent: :destroy
 end
