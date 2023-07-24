f6295bdfd6dd5768644af21919eb0d0add81b966
danielschia <daniel.schia@gmail.com>
2023-07-24 15:53:45 +0100

    set active admin pages

---
+++ app/admin/customers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Customer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :fantasy_name, :customer_name, :tax_id, :status, :state_registration, :operation_ids
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:fantasy_name, :customer_name, :tax_id, :status, :state_registration, :operation_ids]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
